ae0c5d55ffbcd6abf6c527a87a52f17e65cb0b41
aoha0-0 <aquarium.swing@gmail.com>
2026-09-06 16:04:45 +0900

    受付種別から一般販売を削除

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -29,8 +29,7 @@ class Watchlist < ApplicationRecord
     not_set: 0,     # 指定なし
     lottery: 1,     # 抽選受付
     first_come: 2,  # 先着受付
-    made_to_order: 3, # 受注販売
-    general: 4 # 一般販売
+    made_to_order: 3 # 受注販売
   }, default: :not_set
 
   def reception_type_label
